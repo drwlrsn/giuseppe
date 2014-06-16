9a5513678aca8eb79566d9afae0e5ad302d5083e
Drew Larson <drew@drwlrsn.com>
2014-06-16 11:45:56 -0600

    Added debugging to to werzeug middleware when running in a staging environment.

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
+
+if os.environ['FLASK_MODE'] == 'STAGING':
+	from werkzeug.debug import DebuggedApplication
+	app = DebuggedApplication(app, evalex=True)
+
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello, World.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
