41c516bfa7220cecdd9a5ff2ab08bbb39729061e
Drew Larson <drew@drwlrsn.com>
2014-06-18 20:10:24 -0600

    Added basic JSON response for listings.

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
 import os
 from flask import Flask
 from database import db_session
+from models.listing import Listing
+from flask.ext.restful import Resource, Api, fields, marshal_with
 
 app = Flask(__name__)
+api = Api(app)
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World.'
+
+listing_fields = {
+    'id': fields.Integer,
+    'mls_number': fields.Integer,
+    'list_price': fields.Float,
+    'address': fields.String,
+    'images_location_thumbnail': fields.String,
+    'images_number': fields.Integer,
+    'Latitude': fields.Float,
+    'Longitude': fields.Float,
+}
+
+class ListingsRoute(Resource):
+    @marshal_with(listing_fields)
+    def get(self):
+        return db_session.query(Listing).get(1)
+
+api.add_resource(ListingsRoute, '/listing')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
