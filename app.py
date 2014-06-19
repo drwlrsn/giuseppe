2c324096812af63650417624b63f0dc9b1e2f8e2
Drew Larson <drew@drwlrsn.com>
2014-06-19 16:14:23 -0600

    Return all Listing objects.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from database import db_session
 from models.listing import Listing
-from flask.ext.restful import Resource, Api, fields, marshal_with
+from flask.ext.restful import Resource, Api, fields, marshal
 from flask.ext.restful.utils import cors
 
 app = Flask(__name__)
@@ -21,16 +21,16 @@ listing_fields = {
     'address': fields.String,
     'images_location_thumbnail': fields.String,
     'images_number': fields.Integer,
-    'Latitude': fields.Float,
-    'Longitude': fields.Float,
+    'latitude': fields.Float(attribute='Latitude'),
+    'longitude': fields.Float(attribute='Longitude'),
 }
 
 class ListingsRoute(Resource):
-    @marshal_with(listing_fields)
+    
     def get(self):
-        return db_session.query(Listing).get(1)
+        return {'listings': marshal(db_session.query(Listing).all(), listing_fields)}
 
-api.add_resource(ListingsRoute, '/listing')
+api.add_resource(ListingsRoute, '/listings')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
