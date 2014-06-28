8dc0e94e3b5972869c35999aba7ce6ecd62ade50
Drew Larson <drew@drwlrsn.com>
2014-06-28 15:59:53 -0600

    Uh... should have committed this earlier.

--- sync.py
+++ sync.py
@@ -1,13 +1,15 @@
 import librets, os, os.path
 from models.listing import Listing
 from database import db_session, init_db
+from datetime import datetime
+from utils import to_utc
 
 def touch(path):
     with open(path, 'a'):
         os.utime(path, None)
 
 def create_listing_search(session, muid_range='0+', area='S1,S2,S3,S5,S5', \
-                          limit=100):
+                          limit=-1, last_search_datetime=None, select=None):
     """ Using a :class:`librets.RetsSession` perform a :func:`librets.Search`
 
     :param session: Authenticated session object
@@ -26,13 +28,22 @@ def create_listing_search(session, muid_range='0+', area='S1,S2,S3,S5,S5', \
     :returns: A `SearchRequest` to be used in :func:`librets.RetsSession.Search`
     :rtype: :class:`librets.SearchRequest`
     """
+    if not select:
+        select = Listing().select_string
 
-    search_request = session.CreateSearchRequest(\
-            'Property', 'RESI', \
-            '(matrix_unique_ID={muid_range}),(area_name={area_name})'\
-            .format(muid_range=muid_range,area_name=area))
+    if last_search_datetime == None:
+        search_request = session.CreateSearchRequest(\
+                'Property', 'RESI', \
+                '(matrix_unique_ID={muid_range}),(area_name={area_name})'\
+                .format(muid_range=muid_range, area_name=area))
+    else:
+        search_request = session.CreateSearchRequest(\
+                'Property', 'RESI', \
+                '(Matrix_Modified_DT={modified}+),(area_name={area_name})'\
+                .format(modified=last_search_datetime, area_name=area))
+
+    search_request.SetSelect(select)
     search_request.SetLimit(limit)
-    search_request.SetSelect(Listing().select_string)
     search_request.SetCountType(librets.SearchRequest.RECORD_COUNT_AND_RESULTS)
     search_request.SetFormatType(librets.SearchRequest.COMPACT_DECODED)
 
@@ -61,6 +72,38 @@ def create_listing_object_list(results):
 
     return listings_list
 
+def get_all_listings(session, search_request, results=[]):
+    print('get_all_listings() called')
+
+    search_results = create_listing_object_list(session.Search(search_request))
+    if not search_results:
+        return results
+    results += search_results
+
+    return get_all_listings(session, \
+                              create_listing_search(session, \
+                                                    muid_range='{0}+'\
+                                                    .format(int(results[-1]\
+                                                    .matrix_unique_ID) + 1)), \
+                                                    results=results)
+
+def create_listing_muid_list(results):
+    """ Takes the results of :func:`librets.RetsSession.Search` and returns a 
+    list of :class:`Listing` objects ready to be serialised to the database.
+
+    :param results: The results of a :func:`librets.RetsSession.Search`
+    :type results: `librets.SearchResultSet`
+    :returns: A list of `Listing` objects
+    :rtype: list
+    """
+
+    columns = results.GetColumns()
+    listings_list = []
+
+    while results.HasNext():
+        listings_list.append(results.GetString('matrix_unique_ID'))
+
+    return listings_list
 
 def get_listing_images(session, listing):
     """ Given a :class:`Listing` this function downloads all images associated
@@ -120,6 +163,47 @@ def get_listing_images(session, listing):
                 'images_location_{format}'.format(format=image_format.lower()),\
                 dest_dir)
 
+def get_updated_datetime():
+    modified = datetime.fromtimestamp(os.path.getmtime('updated'))
+
+    return to_utc(modified)
+
+def datetime_to_rets_time(datetime_obj):
+    return datetime_obj.strftime('%Y-%m-%dT%H:%M:%S')
+
+def update_listing_table(session, listings):
+    for listing in listings:
+        db_obj = db_session.query(Listing).filter_by(matrix_unique_ID = \
+                                            int(listing.matrix_unique_ID))\
+                                            .first()
+        if db_obj == None:
+            db_session.add(listing)
+        else:
+            db_obj.update(listing)
+            db_session.add(db_obj)
+
+        db_session.commit()
+
+def get_updated_listings(session):
+    """Returns an array of `Listings` that have been updated since 
+    `last_update`
+    """
+    last_updated = datetime_to_rets_time(get_updated_datetime())
+    listing_search = create_listing_search(session, \
+                                           last_search_datetime=last_updated)
+    touch('updated')
+    results = session.Search(listing_search)
+    listing_objs = create_listing_object_list(results)    
+
+    return listing_objs
+
+def create_session():
+    session = librets.RetsSession("http://rets.saskmls.ca/rets/login.ashx")
+    session.SetUserAgentAuthType(librets.RETS_1_5)
+    session.Login('1075', '3L3ctrick!')
+
+    return session
+
 def main():
     """Entry point if called from the command line"""
     touch('updated')
@@ -128,10 +212,12 @@ def main():
     session.SetUserAgentAuthType(librets.RETS_1_5)
     session.Login('1075', '3L3ctrick!')
 
-    listing_search = create_listing_search(session, limit=-1)
-    results = session.Search(listing_search)
+    listings_list = []
 
-    listings = create_listing_object_list(results)
+    while not listings:
+        listing_search = create_listing_search(session, limit=-1)
+        results = session.Search(listing_search)
+        listings = create_listing_object_list(results)
 
     for listing in listings:
         print(listing)
