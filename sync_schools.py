be7282dcfa70357a86c2188b9b073cfb9c2fe8a6
Drew Larson <drew@drwlrsn.com>
2014-11-19 16:21:37 -0600

    Updated for Warman and Martensville

--- sync_schools.py
+++ sync_schools.py
@@ -1,13 +1,29 @@
 import overpass, overpass.execute, overpass.nodes
 from models import School
 import database
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+EXCLUDE_LIST = (
+                'dance',
+                )
 
 def get_schools():
     """Returns a list of school objects"""
     overpass.init()
-    query = 'way[amenity="school"](52.0630454,-106.7706299,52.2067651,-106.5103912);out body;'
-    results = overpass.execute.execute_QL(query)
+    saskatoon_query = 'way[amenity="school"](52.0630454,-106.7706299,52.2067651,-106.5103912);out body;'
+    warman_query = 'way[amenity="school"](52.30491,-106.6092682,52.336388,-106.5591431);out body;'
+    martensville_query = 'way[amenity="school"](52.2757204,-106.681366,52.3059596,-106.636734);out body;'
+
+    results = overpass.execute.execute_QL(saskatoon_query)
     schools = results['elements']
+
+    results = overpass.execute.execute_QL(warman_query)
+    schools += results['elements']
+
+    results = overpass.execute.execute_QL(martensville_query)
+    schools += results['elements']
+
     schools_list = []
     for school in schools:
         try:
@@ -36,3 +52,40 @@ def get_schools():
                             ))
     overpass.end()
     return schools_list
+
+def in_database(school):
+    query = database.session.query(School)
+    return query.filter_by(name=school.name).first()
+
+def excludable(school):
+    for item in EXCLUDE_LIST:
+        if item in school.name.lower():
+            return True
+    return False
+
+def serialise(school, update=False, debug=True):
+    if not excludable(school):
+        if not debug:
+            school_query = in_database(school)
+            if school_query and update:
+                school_query.update(school)
+                database.session.merge(school_query)
+            elif not school_query:
+                database.session.add(school)
+
+            database.session.commit()
+            logging.info('Saved {name} in {city} to database.'.format(name=school.name, city=school.city))
+        else:
+            logging.debug('Saved {name} in {city} to database.'.format(name=school.name, city=school.city))
+            return
+    else:
+        logging.info('Excluding. {name} in {city}.'.format(name=school.name, city=school.city))
+        
+
+def main(debug=True, update=False):
+    schools = get_schools()
+    for school in schools:
+        serialise(school, debug=debug, update=update)
+
+if '__name__' == '__main__':
+    main(debug=False)
